65227b5286653cb19541be5861d13ce5b6c53715
Muhammad Ahmad <Ahmadsaifofficial@gmail.com>
2022-06-14 16:28:20 +0500

    Committing files

---
+++ CodeForces/GNU C++17/1475C | Ball in Berland/154491368.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+void stale_flippant() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+ 
+ 
+int main() {
+    stale_flippant();
+    int t; cin >> t;
+ 
+    while(t--)
+    {
+        int b, g, l;
+        cin >> b >> g >> l;
+        vector<int> boys(b+1, 0), girls(g+1, 0);
+        vector<pair<int, int>> links(l);
+ 
+        for (int i=0; i<l; i++)
+        {
+            cin >> b;
+            boys[b]++;
+            links[i].first = b;
+        }
+ 
+        for (int i=0; i<l; i++)
+        {
+            cin >> b;
+            girls[b]++;
+            links[i].second = b;
+        }
+ 
+        // for (auto c: boys) cout << c << ' ';
+        //     cout << '\n'; 
+        // for (auto c: girls) cout << c << ' ';
+        //     cout << '\n'; 
+        long long ans = 0;
+ 
+        for (auto link : links)
+        {   
+            boys[link.first]--;
+            girls[link.second]--;
+            // cout << boys[link.first] << ' ' << girls[link.second] << '\n';
+            ans += l - (boys[link.first] + girls[link.second]) - 1;
+            boys[link.first]++;
+            girls[link.second]++;
+        }
+ 
+        cout << ans / 2 << '\n';
+    }
+ 
+    return 0;
+    
+}
