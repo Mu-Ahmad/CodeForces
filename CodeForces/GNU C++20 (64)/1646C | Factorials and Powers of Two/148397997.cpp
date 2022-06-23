4887e3c2221c95ff6fe66b51e401f3518c80f072
Muhammad Ahmad <Ahmadsaifofficial@gmail.com>
2022-06-23 16:44:52 +0500

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1646C | Factorials and Powers of Two/148397997.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#define ll long long
+ 
+using namespace std;
+ 
+ 
+void stale_flippant() 
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+ 
+int main()
+{   
+    stale_flippant();
+ 
+    ll fact[17];
+    fact[0] = fact[1] = 1;
+ 
+    for (int i=2; i<17; i++)
+        fact[i] = fact[i-1] * i;
+    
+    int t; cin >> t;
+    while (t--)
+    {
+        ll n; cin >> n;
+        int ans = __builtin_popcountll(n);
+        for (int i=0; i<(1<<15); i++)
+        {
+            ll factorialSum = 0;
+ 
+            for (int p = 0; p <15; p++)
+                if ((1 << p) & i) factorialSum += fact[p];
+ 
+            if (n - factorialSum >= 0)
+            {
+                ll remain = n - factorialSum;
+ 
+                ans = min(ans, __builtin_popcountll(remain) + __builtin_popcount(i));
+            }
+        }
+ 
+        cout << ans << '\n';
+    }
+    return 0;
+}
