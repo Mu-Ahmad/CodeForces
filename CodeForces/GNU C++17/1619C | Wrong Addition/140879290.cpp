78bcc8c2d59836f2e15651eda7f19fdbed006186
Muhammad Ahmad <Ahmadsaifofficial@gmail.com>
2022-06-23 16:47:19 +0500

    Committing files

---
+++ CodeForces/GNU C++17/1619C | Wrong Addition/140879290.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#define deb3(x, y, z)       cout<< #x << ' ' << x << ' '<<#y << ' ' << y << ' ' << #z << ' ' << z << endl
+ 
+using namespace std;
+ 
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ int t; cin >> t;
+ long long a, sum;
+ string b;
+    
+    for (int i=1; i<=t; i++) {
+        cin >> a >> sum;
+  
+  b = "";
+  unsigned long long power = 1;
+  bool possible = true;
+  while (sum) {
+   // deb3(a, b, sum);
+   if (a % 10 > sum % 10) {
+    int temp = sum % 100 - a % 10;
+    a /= 10;
+    sum /= 10;
+    if (sum % 10 != 1) {
+     possible = false;
+     break;
+    }
+    sum /= 10;
+    b  = (char)(temp + '0') + b;
+   } else   {
+    int temp = sum % 10 - a % 10;
+    a /= 10;
+    sum /= 10;
+    b  = (char)(temp + '0') + b;
+    // cout << b << '\n'; 
+   }
+  }
+        
+        istringstream in(b);
+        long long ans; in >> ans;
+  if (!possible or a) cout << "-1\n";
+  else cout << ans << '\n';
+ }
+ 
+ return 0;
+}
