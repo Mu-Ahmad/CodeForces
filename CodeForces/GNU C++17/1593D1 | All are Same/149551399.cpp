57936b6713211f71963845a1766e62f07fefd1e3
Muhammad Ahmad <Ahmadsaifofficial@gmail.com>
2022-06-23 16:42:54 +0500

    Committing files

---
+++ CodeForces/GNU C++17/1593D1 | All are Same/149551399.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#define ll long long
+ 
+using namespace std;
+ 
+ 
+void stale_flippant() 
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+ 
+ 
+int main()
+{   
+    stale_flippant();
+ 
+    int t ; cin >> t;
+ 
+ 
+    while(t--)
+    {   
+        int n; cin >> n;
+        int arr[n];
+        for (int i=0; i<n; i++) cin >> arr[i];
+        int smallest = *min_element(arr, arr+n);
+        int differences[n];
+        for (int i=0; i<n; i++) differences[i] = abs(smallest - arr[i]);
+        int ans = abs(__gcd(differences[0], differences[1]));
+        for (int i=0; i<n; i++) {
+            // cout << differences[i] << '\n';
+            if (ans == 0) ans = differences[i];
+            else ans = min(ans, __gcd(ans, differences[i]));
+        }
+        if (ans == 0) ans--; 
+        cout << ans << '\n';
+    }
+    return 0;
+}
